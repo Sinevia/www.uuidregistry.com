a3a0b4629d39ad0461eda325016a607d62f58769
Sinevia <info@sinevia.com>
2019-09-07 13:52:14 +0100

    Updates

--- shared/js/config.js
+++ shared/js/config.js
@@ -1,2 +1,3 @@
 APP_ID = "97341539c91a43fc9a89026d222baadb";
 WEBSITE_URL = "https://www.uuidregistry.com";
+API_URL = "https://eu-gb.functions.cloud.ibm.com/api/v1/web/sinevia_live/default/uuidregistry.com";
